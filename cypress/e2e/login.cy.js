15b01616dafda0de8b4cb6966743fd29b85f7ec9
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-05-31 21:29:22 +0530

    Redirecting ther user to the given URL

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,14 @@
 describe('spec to launch the lambda test website', () => {
   it('passes', () => {
 
+    //locators
+    const allowCookies = "span[class*='bg-black text-white cursor-pointer hover:underline']";
+
+
     cy.visit('https://www.lambdatest.com/');
     cy.title().should('eq','LAMBDATEST');
 
-  
+    cy.get(allowCookies).click();
 
   })
 })
