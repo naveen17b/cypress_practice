989cdf13005b13de704a772c28abee33444cc295
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-06-13 23:27:28 +0530

    new testpage website URL added for testing

---
+++ cypress/Testcases/Regression/testpages.cy.js
@@ -0,0 +1,22 @@
+
+/**
+ * Testing of the website with the URL: https://testpages.eviltester.com/styled/index.html
+ */
+
+describe('Testing the Testpages website', () => {
+
+    it('Launching the website with the URL', () => {
+
+        cy.visit("https://testpages.eviltester.com/styled/index.html");
+
+    })
+
+    it('Logging in the Swag labs with incorrect login credentials', () => {
+
+
+
+
+
+    })
+})
+
