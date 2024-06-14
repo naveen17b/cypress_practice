e8d639edf817e1b59fda773bd4ec4a360f99e605
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-06-14 22:49:17 +0530

    New command added to execute the E2E testcases, which is showed as not configurewd

---
+++ directE2e.js
@@ -0,0 +1,14 @@
+// this is the command to execute the any E2E program directly
+
+/**
+ * if the person didn't use the cypress for few days , 
+ * then it shows as E2E not configured then if you select the E2E again it will download the E2E files , 
+ * so in order to just skip that part use this command
+ */
+
+
+// ***********************************************************  //
+
+//npx cypress open --e2e
+
+// ***********************************************************  //
