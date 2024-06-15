b50aa9863abeb71b94284bbbcf477cad0558e824
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-06-15 23:10:34 +0530

    added new code regarding launching a zoho demo account page

--- cypress/Testcases/Regression/testpages.cy.js
+++ cypress/Testcases/Regression/testpages.cy.js
@@ -1,22 +1,24 @@
 
 /**
- * Testing of the website with the URL: https://testpages.eviltester.com/styled/index.html
+ * Testing of the website with the URL: https://www.zoho.com/in/books/accounting-software-demo/#/home/dashboard
  */
 
 describe('Testing the Testpages website', () => {
 
     it('Launching the website with the URL', () => {
 
-        cy.visit("https://testpages.eviltester.com/styled/index.html");
+        cy.visit("https://www.zoho.com/in/books/accounting-software-demo/#/home/dashboard", {headers: {"Accept-Encoding": "gzip, deflate"}});
+        //cy.visit(".font-xlarge text-medium mt-3").should('be.a', 'Hello,Demo User');
+
+        //scrolling the sidebar 
+
+        //cy.scrollTo('center');
+        //cy.get('.sidebar').scrollTo('bottom');
+
+        cy.get("li[id='ember99']").click();
+        cy.get("li[id='ember103']").click();
 
     })
 
-    it('Logging in the Swag labs with incorrect login credentials', () => {
-
-
-
-
-
-    })
 })
 
